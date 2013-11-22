34716a08900aeb72b1afb43e627775eca79791db
Ori Yudilevich <oriyudilevich@gmail.com>
2013-11-23 00:23:18 +0100

    Added design elements: controls in slide show, "checks" in reasons, check as favicon, line when hovering over headers and border when hovering over back to top

--- js/script.js
+++ js/script.js
@@ -3,10 +3,12 @@ $(document).ready(function(){
     //mouse over menu items in header
     $('#header_aboutus_a, #header_openinghours_a, #header_location_a, #header_requestappointment_a, #header_payment_a').hover(
         function(){
-        $(this).css('color','red');
+        $(this).css('color','black');
+        $(this).parent().css('border-color','lightgray');
         },
         function(){
         $(this).css('color','black');
+        $(this).parent().css('border-color','white');
     });
 
     
@@ -71,10 +73,12 @@ $(document).ready(function(){
             if (position.top > position_f.top - 10){
                 $('#back_top_container a').css('color','white');
                 $('#back_top_container').css('background','gray');
+                $('#back_top_container').css('border-color','gray');
             }
             else {
                 $('#back_top_container a').css('color','gray');
                 $('#back_top_container').css('background','white');
+                $('#back_top_container').css('border-color','white');
             }
         });
         
@@ -84,20 +88,20 @@ $(document).ready(function(){
             var position = $("#back_top a").offset();
             var position_f = $("#footer").offset();
             if (position.top > position_f.top - 10){
-                $('#back_top_container a').css('color','lightgray');
+                $('#back_top_container').css('border-color','white');
             }
             else {
-                $('#back_top_container a').css('color','red');
+                $('#back_top_container').css('border-color','gray');
             }
             },
             function(){    
             var position = $("#back_top a").offset();
             var position_f = $("#footer").offset();                
             if (position.top > position_f.top - 10){
-                $('#back_top_container a').css('color','white');
+                $('#back_top_container').css('border-color','gray');
             }
             else {
-                $('#back_top_container a').css('color','gray');
+                $('#back_top_container').css('border-color','white');
             }
         });
 
