2d06e2541f1040bbde3e2a70ba20eb23c83f58e6
Misha Veldhoen <misha.veldhoen@gmail.com>
2013-11-25 23:44:33 +0100

    Added google maps popup (still some strange behaviour)

--- js/popup.js
+++ js/popup.js
@@ -4,10 +4,15 @@ jQuery(function($) {
 	// --- Actions ---
 	// ---------------
 	$("a.topopup").click(function() {
-		loadPopup();
+		loadPopup(1);
 		return false;
 	});
 	
+	$("a.mapspopup").click(function() {
+		loadPopup(2);
+		return false;
+	});
+
 	$("div.close").click(function() {
 		disablePopup();
 	});
@@ -22,18 +27,33 @@ jQuery(function($) {
 	
 	var popupOnScreen = 0;
 	
-	function loadPopup() { 
+	function loadPopup(number) { 
 		if(popupOnScreen == 0) {
-			$("#toPopup").fadeIn(0500);
+			switch (number) {
+				case 1:
+					$("#toPopup").fadeIn(0500);
+					break;
+				case 2:
+					$("#mapsPopup").fadeIn(0500);
+					break;	
+			}
+
 			$("#backgroundPopup").css("opacity", "0.7");
 			$("#backgroundPopup").fadeIn(0001); 
-			popupOnScreen = 1;
+			popupOnScreen = number;
 		}	
 	}
 		
 	function disablePopup() {
-		if(popupOnScreen == 1) {
-			$("#toPopup").fadeOut("normal");  
+		if(popupOnScreen) {
+			switch (popupOnScreen) {
+				case 1:
+					$("#toPopup").fadeOut("normal");
+					break;
+				case 2:
+					$("#mapsPopup").fadeOut("normal");
+					break;
+			}  
 			$("#backgroundPopup").fadeOut("normal");  
 			popupOnScreen = 0;
 		}
